5cb7d4206f21382862015f2eb3620d01ed06eb50
Juan Pablo Lagos Moreno <juan.lagos.moreno91@gmail.com>
2023-01-17 23:04:03 -0300

    internando reducers al contexto de usuario

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from 'react'
+import { createContext, useEffect, useReducer } from 'react'
 
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from '../utils/firebase/firebase.utils'
 
@@ -8,9 +8,40 @@ export const UserContext = createContext({
 
 });
 
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER'
+} 
+
+const userReducer = (state, action) => {
+    console.log('dispatched')
+    console.log(action)
+    const { type, payload } = action;
+    
+    switch(type){
+        case USER_ACTION_TYPES.SET_CURRENT_USER :
+            return {
+                ...state,
+                currentUser : payload
+            }
+        default:
+            throw new Error(`unhandled type ${type} in the userReducer`);
+    }
+}
+
+const INITIAL_STATE = {
+    currentUser: null
+}
+
 
 export const UserProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
+    // const [currentUser, setCurrentUser] = useState(null);
+    const [ { currentUser } , dispatch] = useReducer(userReducer, INITIAL_STATE);
+    console.log(currentUser)
+
+    const setCurrentUser = (user) => {
+        dispatch( { type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+    }
+
     const value =  { currentUser, setCurrentUser };  // generar el valor que será un objeto que pase los dos valores que son importantes, que es el user y el setUser.
 
     useEffect(()=>{
